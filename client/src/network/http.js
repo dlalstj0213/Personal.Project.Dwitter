786e326133f2a577dec1ac04fd54cf85ef902355
dlalstj0213 <dlalstj0213@gmail.com>
2021-09-16 16:57:29 +0900

    client: 429 client side considerations codes

--- client/src/network/http.js
+++ client/src/network/http.js
@@ -1,38 +1,61 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+
+const defaultRetryConfig = {
+	retries: 5,
+	initialDelayMs: 100,
+};
+
 export default class HttpClient {
-	constructor(baseURL, authErrorEventBus, getCsrfToken) {
-		this.baseURL = baseURL;
+	constructor(
+		baseURL,
+		authErrorEventBus,
+		getCsrfToken,
+		config = defaultRetryConfig
+	) {
 		this.authErrorEventBus = authErrorEventBus;
 		this.getCsrfToken = getCsrfToken;
+		this.client = axios.create({
+			baseURL: baseURL,
+			headers: { 'Content-Type': 'application/json' },
+			withCredentials: true,
+		});
+		axiosRetry(this.client, {
+			retries: config.retries,
+			retryDelay: (retry) => {
+				const delay = Math.pow(2, retry) * config.initialDelayMs; // 100, 200, 400, 800, 1600
+				const jitter = delay * 0.1 * Math.random();
+				return delay + jitter;
+			},
+			retryCondition: (err) =>
+				axiosRetry.isNetworkOrIdempotentRequestError(err) ||
+				err.response.status === 429,
+		});
 	}
 
 	async fetch(url, options) {
-		const res = await fetch(`${this.baseURL}${url}`, {
-			...options,
+		const { body, method, headers } = options;
+		const req = {
+			url,
+			method,
 			headers: {
-				'Content-Type': 'application/json',
-				...options.headers,
+				...headers,
 				'dwitter-csrf-token': this.getCsrfToken(),
 			},
-			credentials: 'include',
-		});
-		let data;
+			data: body,
+		};
 
 		try {
-			data = await res.json();
-		} catch (error) {
-			console.error(error);
-		}
-
-		if (res.status > 299 || res.status < 200) {
-			const message =
-				data && data.message ? data.message : 'Something went wrong !';
-			const error = new Error(message);
-			if (res.status === 401) {
-				this.authErrorEventBus.notify(error);
-				return;
+			const res = await this.client(req);
+			return res.data;
+		} catch (err) {
+			if (err.response) {
+				const data = err.response.data;
+				const message =
+					data && data.message ? data.message : 'Something went wrong !';
+				throw new Error(message);
 			}
-			throw error;
+			throw new Error('connection error');
 		}
-		return data;
 	}
 }
